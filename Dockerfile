d5c877815ba7d13fbc5cf5171a9f1471e2ce1795
Amir Zadok <amirzadok@gmail.com>
2022-02-13 23:11:13 +0200

    with changes

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,9 @@
-#FROM openjdk:8-jdk-alpine
-#WORKDIR /app
-#ARG JAR_FILE=target/*.jar
-#COPY ${JAR_FILE} app.jar
-#USER node
-#ENV PORT=80
-#EXPOSE 80
-#ENTRYPOINT ["java","-jar","/app.jar"]
-# syntax=docker/dockerfile:1
+FROM openjdk:8-jdk-alpine
 
-# syntax=docker/dockerfile:1
-
-FROM openjdk:16-alpine3.13
-
-WORKDIR /app
-
-COPY .mvn/ .mvn
-COPY mvnw pom.xml ./
-RUN ./mvnw dependency:go-offline
-
-COPY src ./src
-
-CMD ["./mvnw", "spring-boot:run"]
+ENV PORT=80
+EXPOSE 80
+RUN mkdir /logs
+ARG JAR_FILE=target/*.jar
+COPY ${JAR_FILE} app.jar
+CMD ["/bin/sh"]
+ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/rest/assignment/SimpleController.java
+++ src/main/java/com/rest/assignment/SimpleController.java
@@ -2,10 +2,13 @@ package com.rest.assignment;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.document.AbstractXlsView;
 
 
 import java.io.*;
@@ -16,6 +19,7 @@ import java.util.HashMap;
 @RestController
 public class SimpleController {
 
+    private final String ACCESS_TOKEN = "Bearer 1234";
     static Logger LOG =  LoggerFactory.getLogger(SimpleController.class.getName());
 
     @GetMapping(path ="/api/time" , produces=MediaType.APPLICATION_JSON_VALUE)
@@ -27,9 +31,13 @@ public class SimpleController {
         return ResponseEntity.ok(result);
     }
 
-    @GetMapping(path="/api/log" , produces=MediaType.TEXT_HTML_VALUE)
-    public ResponseEntity<Object> getLog() throws IOException {
-        File log = new File("logs\\spring-boot-logger.log");
+    @GetMapping(path="/api/logs" , produces=MediaType.TEXT_HTML_VALUE)
+    public ResponseEntity<Object> getLog(@RequestHeader(value="Authorization")String authValue) throws IOException {
+
+        if (!ACCESS_TOKEN.equals(authValue)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        File log = new File( "logs\\spring-boot-logger.log");
         BufferedReader reader = new BufferedReader(new FileReader(log));
         StringBuilder result = new StringBuilder();
         while(true) {
